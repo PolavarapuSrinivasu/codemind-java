0e721af7a300eb39efe8091d85437c4f96cc2910
Srinivasu <120769051+PolavarapuSrinivasu@users.noreply.github.com>
2024-01-05 11:59:50 +0530

    import java.util.*; public class ReadString {     public static void main(String[] args)     {         Scanner read=new Scanner(System.in);         String s=read.nextLine();         System.out.print(s);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class ReadString
+{
+    public static void main(String[] args)
+    {
+        Scanner read=new Scanner(System.in);
+        String s=read.nextLine();
+        System.out.print(s);
+    }
+}
