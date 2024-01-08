cf592ffe64ec9b208caffc49546a950d9f94aa9e
Srinivasu <120769051+PolavarapuSrinivasu@users.noreply.github.com>
2024-01-08 11:07:30 +0530

    import java.util.*; public class ThirdBoyWeight {     public static void main(String[] args)     {         Scanner read=new Scanner(System.in);         int a=read.nextInt();         float f=a*2.54f;         System.out.printf("%.2f",f);     } }

---
+++ Inches_to_Centimeters.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class ThirdBoyWeight
+{
+    public static void main(String[] args)
+    {
+        Scanner read=new Scanner(System.in);
+        int a=read.nextInt();
+        float f=a*2.54f;
+        System.out.printf("%.2f",f);
+    }
+}
