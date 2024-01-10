6b0939cd7db53acbc236699cae236469e9ff917f
Srinivasu <120769051+PolavarapuSrinivasu@users.noreply.github.com>
2024-01-10 11:10:00 +0530

    import java.util.*; public class KmphToMps {     public static void main(String[] args)     {         Scanner read=new Scanner(System.in);         int n=read.nextInt();         System.out.printf("%.2f",n*0.277778);     } }

---
+++ kmph_to_mps.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class KmphToMps
+{
+    public static void main(String[] args)
+    {
+        Scanner read=new Scanner(System.in);
+        int n=read.nextInt();
+        System.out.printf("%.2f",n*0.277778);
+    }
+}
