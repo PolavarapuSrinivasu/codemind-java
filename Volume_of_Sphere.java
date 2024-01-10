bfc1a4d3a2596caa90866c5c81beccca1a118cf8
Srinivasu <120769051+PolavarapuSrinivasu@users.noreply.github.com>
2024-01-10 11:07:10 +0530

    import java.util.*; public class CanCrossBridge {     public static void main(String[] args)     {         Scanner read=new Scanner(System.in);         int r=read.nextInt();         float f=1.3333333F*3.14F*r*r*r;         System.out.printf("%.2f",f);     } }

---
+++ Volume_of_Sphere.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class CanCrossBridge
+{
+    public static void main(String[] args)
+    {
+        Scanner read=new Scanner(System.in);
+        int r=read.nextInt();
+        float f=1.3333333F*3.14F*r*r*r;
+        System.out.printf("%.2f",f);
+    }
+}
