a8fac19808e9522c560062f1c69ddbe1d4d69bab
Srinivasu <120769051+PolavarapuSrinivasu@users.noreply.github.com>
2024-01-10 11:01:42 +0530

    import java.util.*; public class CanCrossBridge {     public static void main(String[] args)     {         Scanner read=new Scanner(System.in);         int x=read.nextInt();         int y=read.nextInt();         int z=read.nextInt();         System.out.printf("%d",(z-y)/x);     } }

---
+++ Can_Cross_the_Bridge.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class CanCrossBridge
+{
+    public static void main(String[] args)
+    {
+        Scanner read=new Scanner(System.in);
+        int x=read.nextInt();
+        int y=read.nextInt();
+        int z=read.nextInt();
+        System.out.printf("%d",(z-y)/x);
+    }
+}
