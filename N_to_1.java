27a34b4cb33012619ecc90f4cf55ca6a86e826cc
Srinivasu <120769051+PolavarapuSrinivasu@users.noreply.github.com>
2024-01-12 10:56:23 +0530

    import java.util.*; public class SumNaturalNumbers {     public static void main(String[] args)     {         Scanner read=new Scanner(System.in);         int n=read.nextInt();         for(int i=n;i>=1;i--)         {             System.out.printf("%d ",i);         }     } }

---
+++ N_to_1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class SumNaturalNumbers
+{
+    public static void main(String[] args)
+    {
+        Scanner read=new Scanner(System.in);
+        int n=read.nextInt();
+        for(int i=n;i>=1;i--)
+        {
+            System.out.printf("%d ",i);
+        }
+    }
+}
