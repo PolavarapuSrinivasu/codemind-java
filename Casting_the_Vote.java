a1fef4cb46875c42a85b9e99ef3244086228ebc2
Srinivasu <120769051+PolavarapuSrinivasu@users.noreply.github.com>
2024-01-22 09:41:25 +0530

    import java.util.*; public class CastingTheVote {     public static void main(String[] args)     {         Scanner read=new Scanner(System.in);         int a=read.nextInt();         System.out.print(a>=18?"YES":"NO");     } }

---
+++ Casting_the_Vote.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class CastingTheVote
+{
+    public static void main(String[] args)
+    {
+        Scanner read=new Scanner(System.in);
+        int a=read.nextInt();
+        System.out.print(a>=18?"YES":"NO");
+    }
+}
