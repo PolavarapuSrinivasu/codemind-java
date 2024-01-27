308fafce4b51b73a207198c4d8c2bd5f090c8227
Srinivasu <120769051+PolavarapuSrinivasu@users.noreply.github.com>
2024-01-27 11:52:47 +0530

    Add files via upload

---
+++ IntegerLiterals.java
@@ -0,0 +1,18 @@
+public class IntegerLiterals {
+    public static void main(String[] args) {
+        int var1 = 12;
+        System.out.println(var1);
+
+        //0b --> Prefix --> binary Literal (Powers of 2)
+        int var2 = 0b100;
+        System.out.println(var2);
+
+        //0 --> Prefix --> octal (Powers of 8)
+        int var3 = 012;
+        System.out.println(var3); 
+
+        //0x --> Prefix --> hexa Decimal (Powers of 16)
+        int var4 = 0xAF;
+        System.out.println(var4);
+    }
+}
