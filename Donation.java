fbec93fd738ccc758826436bf3e9e887fa4e5e1d
Srinivasu <120769051+PolavarapuSrinivasu@users.noreply.github.com>
2024-02-01 10:45:50 +0530

    import java.util.*; public class Subscription{     public static void main(String args[])     {         Scanner read = new Scanner(System.in);         int n1 = read.nextInt();         int n2 = read.nextInt();         System.out.print(n2 - n1);     } }

---
+++ Donation.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Subscription{
+    public static void main(String args[])
+    {
+        Scanner read = new Scanner(System.in);
+        int n1 = read.nextInt();
+        int n2 = read.nextInt();
+        System.out.print(n2 - n1);
+    }
+}
