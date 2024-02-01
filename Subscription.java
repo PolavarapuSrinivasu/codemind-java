8096f9c18af6d5e0d939251a559fdcb5ca75b5f1
Srinivasu <120769051+PolavarapuSrinivasu@users.noreply.github.com>
2024-02-01 10:43:23 +0530

    import java.util.*; public class Subscription{     public static void main(String args[])     {         Scanner read = new Scanner(System.in);         int n = read.nextInt();         if(n>40) System.out.print("YES");         else System.out.print("NO");     } }

---
+++ Subscription.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Subscription{
+    public static void main(String args[])
+    {
+        Scanner read = new Scanner(System.in);
+        int n = read.nextInt();
+        if(n>40) System.out.print("YES");
+        else System.out.print("NO");
+    }
+}
